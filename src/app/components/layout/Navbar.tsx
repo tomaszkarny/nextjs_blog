3f1ed10d7c1a5409edf1762f8c388ea97fd5430d
Tomasz Karny <karny.tomasz@gmail.com>
2023-11-19 01:52:18 +0100

    added theme switcher

--- src/app/components/layout/Navbar.tsx
+++ src/app/components/layout/Navbar.tsx
@@ -1,5 +1,6 @@
 'use client'
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
+import ThemeSwitcher from '@/src/app/components/layout/ThemeSwitcher'
 import {
   Navbar,
   NavbarBrand,
@@ -13,21 +14,7 @@ import {
 } from '@nextui-org/react'
 
 const NavbarComponent = () => {
-  const [darkMode, setDarkMode] = useState(false);
-
-  useEffect(() => {
-    const root = window.document.documentElement;
-    if (darkMode) {
-      root.classList.add('dark');
-    } else {
-      root.classList.remove('dark');
-    }
-  }, [darkMode]);
-
-  const handleToggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   const menuItems = [
     'Profile',
@@ -92,9 +79,7 @@ const NavbarComponent = () => {
             </Button>
           </NavbarItem>
           <NavbarItem>
-            <Button auto flat aria-label="Toggle Dark Mode" onClick={handleToggleDarkMode}>
-              {darkMode ? 'Light Mode' : 'Dark Mode'}
-            </Button>
+            <ThemeSwitcher />
           </NavbarItem>
         </NavbarContent>
 
@@ -124,3 +109,134 @@ const NavbarComponent = () => {
 }
 
 export default NavbarComponent
+
+// 'use client'
+// import React, { useState, useEffect } from 'react'
+// import ThemeSwitcher from '@/src/app/components/layout/ThemeSwitcher'
+// import {
+//   Navbar,
+//   NavbarBrand,
+//   NavbarMenuToggle,
+//   NavbarMenuItem,
+//   NavbarMenu,
+//   NavbarContent,
+//   NavbarItem,
+//   Link,
+//   Button
+// } from '@nextui-org/react'
+
+// const NavbarComponent = () => {
+//   const [darkMode, setDarkMode] = useState(false)
+
+//   useEffect(() => {
+//     const root = window.document.documentElement
+//     if (darkMode) {
+//       root.classList.add('dark')
+//     } else {
+//       root.classList.remove('dark')
+//     }
+//   }, [darkMode])
+
+//   const handleToggleDarkMode = () => {
+//     setDarkMode(!darkMode)
+//   }
+//   const [isMenuOpen, setIsMenuOpen] = React.useState(false)
+
+//   const menuItems = [
+//     'Profile',
+//     'Dashboard',
+//     'Activity',
+//     'Analytics',
+//     'System',
+//     'Deployments',
+//     'My Settings',
+//     'Team Settings',
+//     'Help & Feedback',
+//     'Log Out'
+//   ]
+
+//   return (
+//     <nav>
+//       <Navbar
+//         isBordered
+//         isMenuOpen={isMenuOpen}
+//         onMenuOpenChange={setIsMenuOpen}
+//       >
+//         <NavbarContent className='sm:hidden' justify='start'>
+//           <NavbarMenuToggle
+//             aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+//           />
+//         </NavbarContent>
+
+//         <NavbarContent className='sm:hidden pr-3' justify='center'>
+//           <NavbarBrand>
+//             <p className='font-bold text-inherit'>My app</p>
+//           </NavbarBrand>
+//         </NavbarContent>
+
+//         <NavbarContent className='hidden sm:flex gap-4' justify='center'>
+//           <NavbarBrand>
+//             <p className='font-bold text-inherit'>My app</p>
+//           </NavbarBrand>
+//           <NavbarItem>
+//             <Link color='foreground' href='/posts'>
+//               Posts
+//             </Link>
+//           </NavbarItem>
+//           <NavbarItem isActive>
+//             <Link href='/' aria-current='page'>
+//               Home
+//             </Link>
+//           </NavbarItem>
+//           <NavbarItem>
+//             <Link color='foreground' href='#'>
+//               Integrations
+//             </Link>
+//           </NavbarItem>
+//         </NavbarContent>
+
+//         <NavbarContent justify='end'>
+//           <NavbarItem className='hidden lg:flex'>
+//             <Link href='#'>Login</Link>
+//           </NavbarItem>
+//           <NavbarItem>
+//             <Button as={Link} color='warning' href='#' variant='flat'>
+//               Sign Up
+//             </Button>
+//           </NavbarItem>
+//           <NavbarItem>
+//             <Button
+//               aria-label='Toggle Dark Mode'
+//               onClick={handleToggleDarkMode}
+//             >
+//               {darkMode ? 'Light Mode' : 'Dark Mode'}
+//             </Button>
+//           </NavbarItem>
+//         </NavbarContent>
+
+//         <NavbarMenu>
+//           {menuItems.map((item, index) => (
+//             <NavbarMenuItem key={`${item}-${index}`}>
+//               <Link
+//                 className='w-full'
+//                 color={
+//                   index === 2
+//                     ? 'warning'
+//                     : index === menuItems.length - 1
+//                     ? 'danger'
+//                     : 'foreground'
+//                 }
+//                 href='#'
+//                 size='lg'
+//               >
+//                 {item}
+//               </Link>
+//             </NavbarMenuItem>
+//           ))}
+//         </NavbarMenu>
+//       </Navbar>
+//     </nav>
+//   )
+// }
+
+// export default NavbarComponent
--- tailwind.config.js
+++ tailwind.config.js
@@ -24,13 +24,70 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       }
-    },
-    darkMode: 'class' // Dodane dla obsługi trybu ciemnego
+    }
+    // darkMode: 'class'
   },
   plugins: [
     require('@tailwindcss/typography'),
-    nextui() // Dodane dla NextUI
+    nextui({
+      themes: {
+        light: {
+          layout: {
+            hoverOpacity: 0.8, //  this value is applied as opacity-[value] when the component is hovered
+            boxShadow: {
+              // shadow-small
+              small:
+                '0px 0px 5px 0px rgb(0 0 0 / 0.02), 0px 2px 10px 0px rgb(0 0 0 / 0.06), 0px 0px 1px 0px rgb(0 0 0 / 0.3)',
+              // shadow-medium
+              medium:
+                '0px 0px 15px 0px rgb(0 0 0 / 0.03), 0px 2px 30px 0px rgb(0 0 0 / 0.08), 0px 0px 1px 0px rgb(0 0 0 / 0.3)',
+              // shadow-large
+              large:
+                '0px 0px 30px 0px rgb(0 0 0 / 0.04), 0px 30px 60px 0px rgb(0 0 0 / 0.12), 0px 0px 1px 0px rgb(0 0 0 / 0.3)'
+            }
+          }, // light theme layout tokens
+          colors: {} // light theme colors
+        },
+        dark: {
+          layout: {}, // dark theme layout tokens
+          colors: {} // dark theme colors
+        },
+        modern: {
+          extend: 'dark', // <- inherit default values from dark theme
+          colors: {
+            background: '#0D001A',
+            foreground: '#ffffff',
+            primary: {
+              50: '#3B096C',
+              100: '#520F83',
+              200: '#7318A2',
+              300: '#9823C2',
+              400: '#c031e2',
+              500: '#DD62ED',
+              600: '#F182F6',
+              700: '#FCADF9',
+              800: '#FDD5F9',
+              900: '#FEECFE',
+              DEFAULT: '#DD62ED',
+              foreground: '#ffffff'
+            },
+            focus: '#F182F6'
+          },
+          layout: {
+            disabledOpacity: '0.3',
+            radius: {
+              small: '1px',
+              medium: '2px',
+              large: '4px'
+            },
+            borderWidth: {
+              small: '1px',
+              medium: '2px',
+              large: '3px'
+            }
+          }
+        }
+      }
+    }) // Dodane dla NextUI
   ]
 }
-
-
