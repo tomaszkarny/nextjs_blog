ad1378d524ce6cb0f6e7c1e0f1af65233987ad2e
Tomasz Karny <karny.tomasz@gmail.com>
2023-12-04 22:50:45 +0100

    Added tailwind.config.js file.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,43 +9,13 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}' // Dodane dla NextUI
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Nunito', 'sans-serif'],
-        serif: ['Merriweather', 'serif']
-      },
-      colors: {
-        primary: '#FF5733',
-        secondary: '#33FF57'
-      },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
-      }
-    }
-    // darkMode: 'class'
-  },
+
   plugins: [
     require('@tailwindcss/typography'),
     nextui({
-      themes: {
+      theme: {
         light: {
-          layout: {
-            hoverOpacity: 0.8, //  this value is applied as opacity-[value] when the component is hovered
-            boxShadow: {
-              // shadow-small
-              small:
-                '0px 0px 5px 0px rgb(0 0 0 / 0.02), 0px 2px 10px 0px rgb(0 0 0 / 0.06), 0px 0px 1px 0px rgb(0 0 0 / 0.3)',
-              // shadow-medium
-              medium:
-                '0px 0px 15px 0px rgb(0 0 0 / 0.03), 0px 2px 30px 0px rgb(0 0 0 / 0.08), 0px 0px 1px 0px rgb(0 0 0 / 0.3)',
-              // shadow-large
-              large:
-                '0px 0px 30px 0px rgb(0 0 0 / 0.04), 0px 30px 60px 0px rgb(0 0 0 / 0.12), 0px 0px 1px 0px rgb(0 0 0 / 0.3)'
-            }
-          }, // light theme layout tokens
+          layout: {}, // light theme layout tokens
           colors: {} // light theme colors
         },
         dark: {
