f88b7c21de6f7f4fd274cdaef0b128a33762cf63
Tomasz Karny <karny.tomasz@gmail.com>
2023-12-26 23:26:08 +0100

    Refactor Hero component to update heading and split text attributes.

--- src/app/components/layout/Hero/Hero.tsx
+++ src/app/components/layout/Hero/Hero.tsx
@@ -5,13 +5,15 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='home-hero_top text-5xl'>
-      <h1 home-hero_heading='true' split-text='true' className='home-hero_heading'>
+      <h1
+        home-hero_heading='true'
+        split-text='true'
+        className='home-hero_heading'
+      >
         <span className='line block text-left w-full'>
-          Trusted Webflow Partners for
+          FIT LIFE TECH INNOVATIONS
         </span>
-        <span className='line block text-left w-full'>
-          your Marketing &amp; Design team
-        </span>
+        <span className='line block text-left w-full'>YOUR DAILY BLOG</span>
       </h1>
     </div>
   )
