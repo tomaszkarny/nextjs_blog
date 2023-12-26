843f527a9bbaf799b1539ae52ba049ee0e45b77e
Tomasz Karny <karny.tomasz@gmail.com>
2023-12-26 23:28:43 +0100

    aider: Add image to the Hero section in `src/app/components/layout/Hero/Hero.tsx`.
    # Aider chat conversation:
    
    ASSISTANT: To add an image to the Hero section, we need to modify `src/app/components/layout/Hero/Hero.tsx`:
    
    Here is the *SEARCH/REPLACE* block:
    
    src/app/components/layout/Hero/Hero.tsx
    ```typescript
    <<<<<<< SEARCH
    const Hero = () => {
      return (
        <div className='home-hero_top text-5xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl'>
          <h1
            home-hero_heading='true'
            split-text='true'
            className='home-hero_heading'
          >
            <span className='line block text-left w-full'>
              Fit Life Tech Innovations
            </span>
            <span className='line block text-left w-full'>Your Daily Blog</span>
          </h1>
        </div>
      )
    }
    =======
    const Hero = () => {
      return (
        <div className='home-hero_top text-5xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl'>
          <img src="/public/2.png" alt="Hero Image" />
          <h1
            home-hero_heading='true'
            split-text='true'
            className='home-hero_heading'
          >
            <span className='line block text-left w-full'>
              Fit Life Tech Innovations
            </span>
            <span className='line block text-left w-full'>Your Daily Blog</span>
          </h1>
        </div>
      )
    }
    >>>>>>> REPLACE
    ```
    This change will add an image to the Hero section in `src/app/components/layout/Hero/Hero.tsx`. The image source is set to `/public/2.png`.

--- src/app/components/layout/Hero/Hero.tsx
+++ src/app/components/layout/Hero/Hero.tsx
@@ -5,6 +5,7 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='home-hero_top text-5xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl'>
+      <img src="/public/2.png" alt="Hero Image" />
       <h1
         home-hero_heading='true'
         split-text='true'
