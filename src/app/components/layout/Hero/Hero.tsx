13d7b335c406fe6176e15ca7cc1ca7b79d1dac85
Tomasz Karny <karny.tomasz@gmail.com>
2023-12-26 23:21:35 +0100

    aider: Replace existing Hero component with new HTML structure.
    # Aider chat conversation:
    
    ASSISTANT: To make this change, we need to modify `src/app/components/layout/Hero/Hero.tsx`:
    
    Here is the *SEARCH/REPLACE* block:
    
    src/app/components/layout/Hero/Hero.tsx
    ```typescript
    <<<<<<< SEARCH
    'use client'
    
    import React from 'react'
    
    const Hero = () => {
      return (
        <div className='foreground'>
          <h1 className='text-6xl font-bold text-foreground'>
            FIT LIFE TECH INNOVATIONS
          </h1>
        </div>
      )
    }
    
    export default Hero
    =======
    'use client'
    
    import React from 'react'
    
    const Hero = () => {
      return (
        <div className='home-hero_top'>
          <h1 home-hero_heading='true' split-text='true' className='home-hero_heading'>
            <span className='line' style={{display: 'block', textAlign: 'start', width: '100%'}}>
              Trusted Webflow Partners for
            </span>
            <span className='line' style={{display: 'block', textAlign: 'start', width: '100%'}}>
              your Marketing &amp; Design team
            </span>
          </h1>
        </div>
      )
    }
    
    export default Hero
    >>>>>>> REPLACE
    ```
    
    This change will replace the existing Hero component in `src/app/components/layout/Hero/Hero.tsx` with a new Hero component that matches the provided HTML structure. Please note that the `home-hero_heading` and `split-text` attributes are not standard HTML attributes and may not have any effect unless they are used in your CSS or JavaScript.

--- src/app/components/layout/Hero/Hero.tsx
+++ src/app/components/layout/Hero/Hero.tsx
@@ -4,9 +4,14 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='foreground'>
-      <h1 className='text-6xl font-bold text-foreground'>
-        FIT LIFE TECH INNOVATIONS
+    <div className='home-hero_top'>
+      <h1 home-hero_heading='true' split-text='true' className='home-hero_heading'>
+        <span className='line' style={{display: 'block', textAlign: 'start', width: '100%'}}>
+          Trusted Webflow Partners for
+        </span>
+        <span className='line' style={{display: 'block', textAlign: 'start', width: '100%'}}>
+          your Marketing &amp; Design team
+        </span>
       </h1>
     </div>
   )
