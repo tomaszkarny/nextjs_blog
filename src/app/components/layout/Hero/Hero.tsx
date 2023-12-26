e33bfe22ec30405330f45cf00a605e92f98ad9e0
Tomasz Karny <karny.tomasz@gmail.com>
2023-12-26 23:33:57 +0100

    aider: Moved the Image component below the h1 tag and added Tailwind CSS classes to make the image responsive.
    # Aider chat conversation:
    
    ASSISTANT: To make the image responsive and place it underneath the text, we need to modify `src/app/components/layout/Hero/Hero.tsx`.
    
    Here are the steps:
    
    1. Move the Image component below the h1 tag.
    2. Add Tailwind CSS classes to make the image responsive.
    
    Here is the *SEARCH/REPLACE block*:
    
    src/app/components/layout/Hero/Hero.tsx
    ```python
    <<<<<<< SEARCH
    const Hero = () => {
      return (
        <div className='home-hero_top text-5xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl'>
          <Image src='/2.png' alt='Hero Image' width={500} height={300} />
          <h1
            home-hero_heading='true'
            split-text='true'
            className='home-hero_heading'
          >
            <span className='line block text-left w-full'>
              Fit Life Tech Innovations
            </span>
            <span className='line block text-left w-full'>Your Daily Blog</span>
          </h1>
        </div>
      )
    }
    =======
    const Hero = () => {
      return (
        <div className='home-hero_top text-5xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl'>
          <h1
            home-hero_heading='true'
            split-text='true'
            className='home-hero_heading'
          >
            <span className='line block text-left w-full'>
              Fit Life Tech Innovations
            </span>
            <span className='line block text-left w-full'>Your Daily Blog</span>
          </h1>
          <Image src='/2.png' alt='Hero Image' className='w-full h-auto' />
        </div>
      )
    }
    >>>>>>> REPLACE
    ```
    
    In the `REPLACE` block, I've moved the `Image` component below the `h1` tag and added the `w-full h-auto` classes to make the image responsive. The `w-full` class sets the width of the image to 100% of its parent container, and the `h-auto` class sets the height of the image to auto, maintaining the aspect ratio of the image.

--- src/app/components/layout/Hero/Hero.tsx
+++ src/app/components/layout/Hero/Hero.tsx
@@ -4,7 +4,6 @@ import Image from 'next/image'
 const Hero = () => {
   return (
     <div className='home-hero_top text-5xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl'>
-      <Image src='/2.png' alt='Hero Image' width={500} height={300} />
       <h1
         home-hero_heading='true'
         split-text='true'
@@ -15,6 +14,7 @@ const Hero = () => {
         </span>
         <span className='line block text-left w-full'>Your Daily Blog</span>
       </h1>
+      <Image src='/2.png' alt='Hero Image' className='w-full h-auto' />
     </div>
   )
 }
