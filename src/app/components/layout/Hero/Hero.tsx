4fcf80c8dd18a29231b8c0b654e7870ea104fd71
Tomasz Karny <karny.tomasz@gmail.com>
2023-12-26 23:42:29 +0100

    aider: Change the layout to display the div on the left and the image on the right using Tailwind CSS.
    # Aider chat conversation:
    
    USER: this div must be on the left side the image on the right side style it with tailwind
    
    ASSISTANT: To make the div appear on the left side and the image on the right side, we need to modify the `src/app/components/layout/Hero/Hero.tsx` file. We will use Tailwind CSS's Flexbox utilities to create a horizontal layout.
    
    Here is the *SEARCH/REPLACE* block:
    
    src/app/components/layout/Hero/Hero.tsx
    ```python
    <<<<<<< SEARCH
    <div className='home-hero_top text-5xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl'>
      <h1
        home-hero_heading='true'
        split-text='true'
        className='home-hero_heading mb-8'
      >
        <span className='line block text-left w-full'>
          Fit Life Tech Innovations
        </span>
        <span className='line block text-left w-full'>Your Daily Blog</span>
      </h1>
      <div className="margin-bottom margin-small">
        <p split-text="true">
          <span className="line" style={{display: "block", textAlign: "start", width: "100%"}}>We help medium-to-large companies</span>
          <span className="line" style={{display: "block", textAlign: "start", width: "100%"}}>and agencies build highly scalable</span>
          <span className="line" style={{display: "block", textAlign: "start", width: "100%"}}>Webflow websites and help them do</span>
          <span className="line" style={{display: "block", textAlign: "start", width: "100%"}}>more in less time.</span>
        </p>
      </div>
      <Image
        src='/2.png'
        alt='Hero Image'
        width={500}
        height={300}
        className='w-full h-auto'
      />
    </div>
    =======
    <div className='home-hero_top flex text-5xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl'>
      <div>
        <h1
          home-hero_heading='true'
          split-text='true'
          className='home-hero_heading mb-8'
        >
          <span className='line block text-left w-full'>
            Fit Life Tech Innovations
          </span>
          <span className='line block text-left w-full'>Your Daily Blog</span>
        </h1>
        <div className="margin-bottom margin-small">
          <p split-text="true">
            <span className="line" style={{display: "block", textAlign: "start", width: "100%"}}>We help medium-to-large companies</span>
            <span className="line" style={{display: "block", textAlign: "start", width: "100%"}}>and agencies build highly scalable</span>
            <span className="line" style={{display: "block", textAlign: "start", width: "100%"}}>Webflow websites and help them do</span>
            <span className="line" style={{display: "block", textAlign: "start", width: "100%"}}>more in less time.</span>
          </p>
        </div>
      </div>
      <Image
        src='/2.png'
        alt='Hero Image'
        width={500}
        height={300}
        className='w-full h-auto ml-auto'
      />
    </div>
    >>>>>>> REPLACE
    ```
    
    This change will make the div and the image appear side by side, with the div on the left and the image on the right. The `flex` class is used to create a flex container, and the `ml-auto` class is used to push the image to the right side of the container.

--- src/app/components/layout/Hero/Hero.tsx
+++ src/app/components/layout/Hero/Hero.tsx
@@ -3,31 +3,33 @@ import Image from 'next/image'
 
 const Hero = () => {
   return (
-    <div className='home-hero_top text-5xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl'>
-      <h1
-        home-hero_heading='true'
-        split-text='true'
-        className='home-hero_heading mb-8'
-      >
-        <span className='line block text-left w-full'>
-          Fit Life Tech Innovations
-        </span>
-        <span className='line block text-left w-full'>Your Daily Blog</span>
-      </h1>
-      <div className="margin-bottom margin-small">
-        <p split-text="true">
-          <span className="line" style={{display: "block", textAlign: "start", width: "100%"}}>We help medium-to-large companies</span>
-          <span className="line" style={{display: "block", textAlign: "start", width: "100%"}}>and agencies build highly scalable</span>
-          <span className="line" style={{display: "block", textAlign: "start", width: "100%"}}>Webflow websites and help them do</span>
-          <span className="line" style={{display: "block", textAlign: "start", width: "100%"}}>more in less time.</span>
-        </p>
+    <div className='home-hero_top flex text-5xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl'>
+      <div>
+        <h1
+          home-hero_heading='true'
+          split-text='true'
+          className='home-hero_heading mb-8'
+        >
+          <span className='line block text-left w-full'>
+            Fit Life Tech Innovations
+          </span>
+          <span className='line block text-left w-full'>Your Daily Blog</span>
+        </h1>
+        <div className="margin-bottom margin-small">
+          <p split-text="true">
+            <span className="line" style={{display: "block", textAlign: "start", width: "100%"}}>We help medium-to-large companies</span>
+            <span className="line" style={{display: "block", textAlign: "start", width: "100%"}}>and agencies build highly scalable</span>
+            <span className="line" style={{display: "block", textAlign: "start", width: "100%"}}>Webflow websites and help them do</span>
+            <span className="line" style={{display: "block", textAlign: "start", width: "100%"}}>more in less time.</span>
+          </p>
+        </div>
       </div>
       <Image
         src='/2.png'
         alt='Hero Image'
         width={500}
         height={300}
-        className='w-full h-auto'
+        className='w-full h-auto ml-auto'
       />
     </div>
   )
