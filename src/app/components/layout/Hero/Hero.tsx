cfb073760e4c0fc85971eb4699a1d110ef1a8e92
Tomasz Karny <karny.tomasz@gmail.com>
2023-12-26 23:21:33 +0100

    Refactor Hero component to use single quotes and update class names.

--- src/app/components/layout/Hero/Hero.tsx
+++ src/app/components/layout/Hero/Hero.tsx
@@ -4,8 +4,10 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className="bg-background">
-      <h1 className="text-6xl font-bold text-foreground">FIT LIFE TECH INNOVATIONS</h1>
+    <div className='foreground'>
+      <h1 className='text-6xl font-bold text-foreground'>
+        FIT LIFE TECH INNOVATIONS
+      </h1>
     </div>
   )
 }
