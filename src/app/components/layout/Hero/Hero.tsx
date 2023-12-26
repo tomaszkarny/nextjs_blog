13287e0870e25ae99db3d00c1db0b5a72667146a
Tomasz Karny <karny.tomasz@gmail.com>
2023-12-26 23:29:32 +0100

    aider: Optimized the image in the Hero section by replacing the <img> tag with the <Image /> component from 'next/image'.
    # Aider chat conversation:
    
    USER: Using `<img>` could result in slower LCP and higher bandwidth. Consider using `<Image />` from `next/image` to automatically optimize images. This may incur additional usage or cost from your provider. See: https://nextjs.org/docs/messages/no-img-elementeslint@next/next/no-img-element
    
    ASSISTANT: To optimize the image in the Hero section, we need to modify `src/app/components/layout/Hero/Hero.tsx`:
    
    1. Import the Image component from 'next/image'.
    2. Replace the `<img>` tag with the `<Image />` component from 'next/image'.
    
    Here is the *SEARCH/REPLACE* block:
    
    src/app/components/layout/Hero/Hero.tsx
    ```typescript
    <<<<<<< SEARCH
    'use client'
    
    import React from 'react'
    
    const Hero = () => {
      return (
        <div className='home-hero_top text-5xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl'>
          <img src="/public/2.png" alt="Hero Image" />
    =======
    import React from 'react'
    import Image from 'next/image'
    
    const Hero = () => {
      return (
        <div className='home-hero_top text-5xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl'>
          <Image src="/public/2.png" alt="Hero Image" width={500} height={300} />
    >>>>>>> REPLACE
          <h1
            home-hero_heading='true'
            split-text='true'
            className='home-hero_heading'
          >
            <span className='line block text-left w-full'>
              Fit Life Tech Innovations
            </span>
            <span className='line block text-left w-full'>Your Daily Blog</span>
          </h1>
        </div>
      )
    }
    
    export default Hero
    ```
    
    This change will replace the `<img>` tag with the `<Image />` component from 'next/image' in `src/app/components/layout/Hero/Hero.tsx`. The Image component automatically optimizes images for better performance. Please adjust the width and height attributes as needed.

--- src/app/components/layout/Hero/Hero.tsx
+++ src/app/components/layout/Hero/Hero.tsx
@@ -1,11 +1,10 @@
-'use client'
-
 import React from 'react'
+import Image from 'next/image'
 
 const Hero = () => {
   return (
     <div className='home-hero_top text-5xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl'>
-      <img src="/public/2.png" alt="Hero Image" />
+      <Image src="/public/2.png" alt="Hero Image" width={500} height={300} />
       <h1
         home-hero_heading='true'
         split-text='true'
