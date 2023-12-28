e34f1b6fe3f1bd6f47ab117999679e6d31cb3ff7
Tomasz Karny <karny.tomasz@gmail.com>
2023-12-28 19:40:15 +0100

    aider: Refactored Hero component to use Tailwind CSS's responsive design features for different screen sizes.
    # Aider chat conversation:
    
    ASSISTANT: To make the Hero component fully responsive, we need to modify the CSS classes in the `Hero.tsx` file. We will use Tailwind CSS's responsive design features, which allow us to specify different classes for different screen sizes.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    /Users/tommy/Desktop/nextjs_blog/src/app/components/layout/Hero/Hero.tsx
    ```tsx
    <<<<<<< SEARCH
    <div className='flex flex-col min-h-screen'>
      <div className='p-10 md:p-20'>
        <h1 className='text-5xl font-bold mb-4 leading-tight'>
          Trusted Webflow Partners for your Marketing & Design team
        </h1>
      </div>
      <div className='flex flex-1 flex-col md:flex-row'>
        <div className='flex flex-col justify-center p-10 md:p-20 md:w-1/2'>
          <p className='text-lg mb-4'>
            We help medium-to-large companies and agencies build highly scalable
            Webflow websites and help them do more in less time.
          </p>
          <button className='btn-primary self-start'>START A PROJECT</button>
          {/* Replace with your button component */}
        </div>
        <div className='md:w-1/2 relative'>
          <Image
            src='/2.png'
            alt='Hero Image'
            layout='fill'
            objectFit='contain' // Use 'cover' to ensure the image fully covers the div
            priority // Use priority to hint to the browser that this is an important image
          />
        </div>
      </div>
    </div>
    =======
    <div className='flex flex-col min-h-screen'>
      <div className='p-4 sm:p-6 md:p-8 lg:p-10 xl:p-20'>
        <h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold mb-4 leading-tight'>
          Trusted Webflow Partners for your Marketing & Design team
        </h1>
      </div>
      <div className='flex flex-1 flex-col sm:flex-row'>
        <div className='flex flex-col justify-center p-4 sm:p-6 md:p-8 lg:p-10 xl:p-20 w-full sm:w-1/2'>
          <p className='text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl mb-4'>
            We help medium-to-large companies and agencies build highly scalable
            Webflow websites and help them do more in less time.
          </p>
          <button className='btn-primary self-start'>START A PROJECT</button>
          {/* Replace with your button component */}
        </div>
        <div className='w-full sm:w-1/2 relative'>
          <Image
            src='/2.png'
            alt='Hero Image'
            layout='fill'
            objectFit='contain' // Use 'cover' to ensure the image fully covers the div
            priority // Use priority to hint to the browser that this is an important image
          />
        </div>
      </div>
    </div>
    >>>>>>> REPLACE
    ```
    
    This change will make the padding, font size, and layout responsive to different screen sizes. The classes `sm:`, `md:`, `lg:`, and `xl:` are used to apply styles for small, medium, large, and extra large screens, respectively.

--- src/app/components/layout/Hero/Hero.tsx
+++ src/app/components/layout/Hero/Hero.tsx
@@ -4,21 +4,21 @@ import Image from 'next/image'
 const Hero = () => {
   return (
     <div className='flex flex-col min-h-screen'>
-      <div className='p-10 md:p-20'>
-        <h1 className='text-5xl font-bold mb-4 leading-tight'>
+      <div className='p-4 sm:p-6 md:p-8 lg:p-10 xl:p-20'>
+        <h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold mb-4 leading-tight'>
           Trusted Webflow Partners for your Marketing & Design team
         </h1>
       </div>
-      <div className='flex flex-1 flex-col md:flex-row'>
-        <div className='flex flex-col justify-center p-10 md:p-20 md:w-1/2'>
-          <p className='text-lg mb-4'>
+      <div className='flex flex-1 flex-col sm:flex-row'>
+        <div className='flex flex-col justify-center p-4 sm:p-6 md:p-8 lg:p-10 xl:p-20 w-full sm:w-1/2'>
+          <p className='text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl mb-4'>
             We help medium-to-large companies and agencies build highly scalable
             Webflow websites and help them do more in less time.
           </p>
           <button className='btn-primary self-start'>START A PROJECT</button>
           {/* Replace with your button component */}
         </div>
-        <div className='md:w-1/2 relative'>
+        <div className='w-full sm:w-1/2 relative'>
           <Image
             src='/2.png'
             alt='Hero Image'
