7cca90c90b93abea8d7fcec0d0870bce4d6c59d3
Tomasz Karny <karny.tomasz@gmail.com>
2023-12-28 20:21:48 +0100

    aider: Fix aspect ratio by setting width to "auto" in Hero component.
    # Aider chat conversation:
    
    ASSISTANT: To fix this warning, we need to modify `src/app/components/layout/Hero/Hero.tsx`:
    
    1. Add 'width: "auto"' to the Image component to maintain the aspect ratio.
    
    Here is the *SEARCH/REPLACE* block:
    
    src/app/components/layout/Hero/Hero.tsx
    ```tsx
    <<<<<<< SEARCH
    <Image
      src='/2.png'
      alt='Hero Image'
      width={500} // Add the width prop
      height={400} // Decrease the height prop
      objectFit='contain' // Use 'cover' to ensure the image fully covers the div
      priority // Use priority to hint to the browser that this is an important image
      sizes='(max-width: 640px) 100vw, (max-width: 768px) 100vw, (max-width: 1024px) 100vw, 50vw'
    />
    =======
    <Image
      src='/2.png'
      alt='Hero Image'
      width='auto' // Change width to 'auto'
      height={400} // Keep the height prop
      objectFit='contain' // Use 'cover' to ensure the image fully covers the div
      priority // Use priority to hint to the browser that this is an important image
      sizes='(max-width: 640px) 100vw, (max-width: 768px) 100vw, (max-width: 1024px) 100vw, 50vw'
    />
    >>>>>>> REPLACE
    ```
    
    This change will maintain the aspect ratio of the image.

--- src/app/components/layout/Hero/Hero.tsx
+++ src/app/components/layout/Hero/Hero.tsx
@@ -19,8 +19,8 @@ const Hero = () => {
           <Image
             src='/2.png'
             alt='Hero Image'
-            width={500} // Add the width prop
-            height={400} // Decrease the height prop
+            width='auto' // Change width to 'auto'
+            height={400} // Keep the height prop
             objectFit='contain' // Use 'cover' to ensure the image fully covers the div
             priority // Use priority to hint to the browser that this is an important image
             sizes='(max-width: 640px) 100vw, (max-width: 768px) 100vw, (max-width: 1024px) 100vw, 50vw'
