ea8b4b67919414af8a8585bbf481d7a33c390e95
Tomasz Karny <karny.tomasz@gmail.com>
2023-12-28 19:44:05 +0100

    aider: Ensure image display on mobile view and reduce gap between h1 and p tags on tablet view.
    # Aider chat conversation:
    
    USER: kiedy zmniejsza się szerokość zdjęcie i tekst idą w dół przy mobile view w ogóle zdjęcie się nie pokazuje przy szerokosci ekranu na tablety jest za  duzy "gap" między h1 a <p>
    
    ASSISTANT: To address the issues you've mentioned, we need to modify the `Hero.tsx` file:
    
    1. To ensure the image is displayed on mobile view, we can remove the `sm:w-1/2` class from the image container div. This will make the image take the full width on all screen sizes.
    2. To reduce the gap between the h1 and p tags on tablet view, we can reduce the padding on larger screens by changing `xl:p-20` to `xl:p-10` on the div containing the text.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    src/app/components/layout/Hero/Hero.tsx
    ```tsx
    <<<<<<< SEARCH
    <div className='flex flex-1 flex-col sm:flex-row'>
      <div className='flex flex-col justify-center p-4 sm:p-6 md:p-8 lg:p-10 xl:p-20 w-full sm:w-1/2'>
        <p className='text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl mb-4'>
          We help medium-to-large companies and agencies build highly scalable
          Webflow websites and help them do more in less time.
        </p>
        <button className='btn-primary self-start'>START A PROJECT</button>
        {/* Replace with your button component */}
      </div>
      <div className='w-full sm:w-1/2 relative'>
        <Image
          src='/2.png'
          alt='Hero Image'
          layout='fill'
          objectFit='contain' // Use 'cover' to ensure the image fully covers the div
          priority // Use priority to hint to the browser that this is an important image
        />
      </div>
    </div>
    =======
    <div className='flex flex-1 flex-col sm:flex-row'>
      <div className='flex flex-col justify-center p-4 sm:p-6 md:p-8 lg:p-10 xl:p-10 w-full sm:w-1/2'>
        <p className='text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl mb-4'>
          We help medium-to-large companies and agencies build highly scalable
          Webflow websites and help them do more in less time.
        </p>
        <button className='btn-primary self-start'>START A PROJECT</button>
        {/* Replace with your button component */}
      </div>
      <div className='w-full relative'>
        <Image
          src='/2.png'
          alt='Hero Image'
          layout='fill'
          objectFit='contain' // Use 'cover' to ensure the image fully covers the div
          priority // Use priority to hint to the browser that this is an important image
        />
      </div>
    </div>
    >>>>>>> REPLACE
    ```
    
    This change will ensure the image is displayed on mobile view and reduce the gap between the h1 and p tags on tablet view.

--- src/app/components/layout/Hero/Hero.tsx
+++ src/app/components/layout/Hero/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
         </h1>
       </div>
       <div className='flex flex-1 flex-col sm:flex-row'>
-        <div className='flex flex-col justify-center p-4 sm:p-6 md:p-8 lg:p-10 xl:p-20 w-full sm:w-1/2'>
+        <div className='flex flex-col justify-center p-4 sm:p-6 md:p-8 lg:p-10 xl:p-10 w-full sm:w-1/2'>
           <p className='text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl mb-4'>
             We help medium-to-large companies and agencies build highly scalable
             Webflow websites and help them do more in less time.
@@ -18,7 +18,7 @@ const Hero = () => {
           <button className='btn-primary self-start'>START A PROJECT</button>
           {/* Replace with your button component */}
         </div>
-        <div className='w-full sm:w-1/2 relative'>
+        <div className='w-full relative'>
           <Image
             src='/2.png'
             alt='Hero Image'
