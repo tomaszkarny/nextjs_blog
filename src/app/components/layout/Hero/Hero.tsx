f708160be34abedc9962b46ccffbeb6bfb8c6516
Tomasz Karny <karny.tomasz@gmail.com>
2023-12-28 19:53:03 +0100

    aider: Changed h1 and p tags to use responsive font sizes.
    # Aider chat conversation:
    
    USER: w miarę jak ekran się zmniejsza dostosowuj wielkość h1 oraz p adekwatnie zmniejszając
    
    ASSISTANT: To make the h1 and p tags responsive, we need to modify the `src/app/components/layout/Hero/Hero.tsx` file:
    
    1. Change the className of the h1 tag to use responsive font sizes.
    2. Change the className of the p tag to use responsive font sizes.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    src/app/components/layout/Hero/Hero.tsx
    ```tsx
    <<<<<<< SEARCH
    <h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold mb-4 leading-tight'>
    =======
    <h1 className='text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold mb-4 leading-tight'>
    >>>>>>> REPLACE
    ```
    
    src/app/components/layout/Hero/Hero.tsx
    ```tsx
    <<<<<<< SEARCH
    <p className='text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl mb-4'>
    =======
    <p className='text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl mb-4'>
    >>>>>>> REPLACE
    ```
    
    These changes will make the h1 and p tags responsive, adjusting their size as the screen size changes.

--- src/app/components/layout/Hero/Hero.tsx
+++ src/app/components/layout/Hero/Hero.tsx
@@ -5,13 +5,13 @@ const Hero = () => {
   return (
     <div className='flex flex-col min-h-screen'>
       <div className='p-4 sm:p-6 md:p-8 lg:p-10 xl:p-20'>
-        <h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold mb-4 leading-tight'>
+        <h1 className='text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold mb-4 leading-tight'>
           Trusted Webflow Partners for your Marketing & Design team
         </h1>
       </div>
       <div className='flex flex-1 flex-col md:flex-row'>
         <div className='flex flex-col justify-center p-4 sm:p-6 md:p-8 lg:p-10 xl:p-10 w-full'>
-          <p className='text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl mb-4'>
+          <p className='text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl mb-4'>
             We help medium-to-large companies and agencies build highly scalable
             Webflow websites and help them do more in less time.
           </p>
