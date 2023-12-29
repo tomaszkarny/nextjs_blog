98f28cba30c9b51eb99821a19b14d5b3945016af
Tomasz Karny <karny.tomasz@gmail.com>
2023-12-30 00:25:33 +0100

    aider: Refactor Hero component styles for better readability and consistency.
    # Aider chat conversation:
    
    ASSISTANT: No changes are needed.

--- src/app/components/layout/Hero/Hero.tsx
+++ src/app/components/layout/Hero/Hero.tsx
@@ -7,12 +7,12 @@ const Hero = () => {
   return (
     <div className='flex flex-col min-h-screen'>
       <div className='p-3 sm:p-6 md:p-8 lg:p-10 xl:p-20'>
-        <h1 className=' sm:text-3xl md:text-4xl lg:text-5xl xl:text-7xl font-bold mb-5 leading-tight'>
+        <h1 className='sm:text-3xl md:text-4xl lg:text-5xl xl:text-7xl font-bold mb-5 leading-tight'>
           Trusted Webflow Partners for your Marketing & Design team
         </h1>
       </div>
       <div className='flex flex-1 flex-col md:flex-row'>
-        <div className='flex flex-col justify-center p-4 sm:p-4 md:p-10 md:h-fit  lg:p-8 xl:p-10 w-full h-58'>
+        <div className='flex flex-col justify-center p-4 sm:p-4 md:p-10 md:h-fit lg:p-8 xl:p-10 w-full h-58'>
           <p className='text-xs sm:text-sm md:2xl lg:text-5 xl:text-xl mb-4 h-24'>
             We help medium-to-large companies and agencies build highly scalable
             Webflow websites and help them do more in less time.
@@ -20,7 +20,7 @@ const Hero = () => {
           <div className='flex flex-wrap gap-4 items-center justify-center'>
             <Button
               radius='lg'
-              size='lg'
+              size={['lg', 'md', 'sm']}
               className='bg-gradient-to-tr from-darkSlateGrey to-darkSeaGreen text-green shadow-lg'
             >
               Button
