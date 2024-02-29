88db23c6461d25086e7377734aec43dfb403c9c9
Tomasz Karny <karny.tomasz@gmail.com>
2024-02-29 12:43:29 +0100

    Updated Hero component

--- src/app/components/layout/Hero/Hero.tsx
+++ src/app/components/layout/Hero/Hero.tsx
@@ -1,37 +1,42 @@
-'use client'
-import React from 'react'
+import React, { useState, useMemo } from 'react'
 import Image from 'next/image'
-import heroImg from '@/public/heroImg.png'
-import { Button } from '@nextui-org/react'
+import { Button, Input } from '@nextui-org/react'
+import NewsletterForm from '@/src/app/components/forms/NewsletterForm'
+
+import medium from '@/public/medium.jpeg'
 
 const Hero = () => {
+  const frameStyle = {
+    border: '3px solid #d0edd6',
+    boxShadow: '20 20 10px rgba(45, 38, 38, 0.3)',
+    clipPath:
+      'polygon(10% 0%, 90% 0%, 100% 10%, 100% 90%, 90% 100%, 10% 100%, 0% 90%, 0% 10%)',
+    transition: 'box-shadow 0.3s ease, transform 0.3s ease'
+  }
   return (
-    <div className='flex flex-col pb-8'>
+    <div className=' flex flex-col pb-8 items-center'>
       <div className='p-3 sm:p-6 md:p-8 lg:p-10 xl:p-15'>
-        <h1 className='text-3xl sm:text-4xl md:text-4xl lg:text-5xl xl:text-7xl font-bold mb-2 leading-tight'>
-          Trusted Webflow Partners for your Marketing & Design team
+        <h1 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold mb-4 leading-tight'>
+          Navigating the Future of Wellbeing
         </h1>
+        <h2 className='text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-light mb-2'>
+          Welcome to the Tech-Enhanced Fitness Blog
+        </h2>
       </div>
-      <div className='flex flex-1 flex-col md:flex-row items-center'>
-        <div className='flex flex-col justify-center p-4 sm:p-4 md:p-10 md:h-fit  lg:p-8 xl:p-10 w-full h-58'>
+      <div className='flex flex-1 flex-col md:flex-row '>
+        <div className='flex flex-col justify-center p-4  sm:p-4 items-baseline  sm:items-center md:p-10 md:h-fit  lg:p-8 xl:p-10 w-full h-58'>
           <p className='text-xs sm:text-sm md:2xl lg:text-5 xl:text-xl mb-4 h-24'>
-            We help medium-to-large companies and agencies build highly scalable
-            Webflow websites and help them do more in less time.
+            Explore our latest fitness tips and articles to help you achieve
+            your health goals. Join our community today!
           </p>
-          <div className='flex flex-wrap gap-4 items-center justify-center'>
-            <Button
-              radius='sm'
-              size='lg'
-              className='bg-darkSlateGrey  text-primaryColorDark shadow-lg text-center tracking-wide uppercase  font-medium leading-snug no-underline px-6 py-3 w-64 text-xs sm:text-sm md:text-sm lg:text-lg'
-            >
-              START A PROJECT
-            </Button>
-          </div>
+          <NewsletterForm />
         </div>
-
-        <div className='w-full h-full relative md:block  mb-4 sm:mb-0'>
+        <div
+          className='w-full h-full relative md:block mb-4 sm:mb-0 rounded-lg overflow-hidden '
+          style={frameStyle}
+        >
           <Image
-            src={heroImg}
+            src={medium}
             alt='Hero Image'
             width={1200}
             height={800}
