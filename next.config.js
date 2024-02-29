a26080e5e8caa968c0c418041a938d112841ef95
Tomasz Karny <karny.tomasz@gmail.com>
2024-02-29 18:57:48 +0100

    chore: update project configuration and dependencies

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
     CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
     CONTENTFUL_PREVIEW_ACCESS_TOKEN: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack']
+    })
+
+    return config
   }
 }
 
